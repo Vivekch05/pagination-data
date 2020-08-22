41f2055c8a8d86be8f1be7cbcb02b79be93593c8
Vivek Kumar Chaurasia <vivek.chaurasia@synapse.com>
2020-08-22 10:52:13 +0530

    Updates

---
+++ precache-manifest.9bf753b3d18140d066249ec0cc52c2a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e317cb5fe2b4f446fe3084ca0f80ce72",
+    "url": "/pagination-data/index.html"
+  },
+  {
+    "revision": "74db978ba23d5e803bab",
+    "url": "/pagination-data/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe487a3d69a1038df1c6",
+    "url": "/pagination-data/static/js/2.55b874b8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pagination-data/static/js/2.55b874b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74db978ba23d5e803bab",
+    "url": "/pagination-data/static/js/main.59efc4a8.chunk.js"
+  },
+  {
+    "revision": "6bd436d2694e0d6d816a",
+    "url": "/pagination-data/static/js/runtime-main.61ab35a4.js"
+  }
+]);
